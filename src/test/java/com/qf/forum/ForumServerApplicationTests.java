a5ad471fd50c55ef6833da436bccae2209748b33
Liewona <40515807+Liewona@users.noreply.github.com>
2020-11-03 00:16:16 +0800

    validate cancel commit

--- src/test/java/com/qf/forum/ForumServerApplicationTests.java
+++ src/test/java/com/qf/forum/ForumServerApplicationTests.java
@@ -3,6 +3,9 @@ package com.qf.forum;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @SpringBootTest
 class ForumServerApplicationTests {
 
